2c24151d2d71e321f7daa365143c6729bd4a4721
Fightingkeyboard <31511749+Fightingkeyboard@users.noreply.github.com>
2017-09-25 00:18:36 -0400

    Comma Code revision

--- 04/Comma Code.py
+++ 04/Comma Code.py
@@ -3,7 +3,7 @@ def Comma_Code(x):
     for items in range(len(x) -1):
         y += str(x[items])+', '
     z= 'and ' + str(x[-1])
-    print (y+z)
+    return (y+z)
     
 l = [1,2,3,4,5,6,7,8,9,0]
-Comma_Code(l)
+print (Comma_Code(l))
