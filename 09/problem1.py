0bd4bdcc9beae334ab14e074acd8fe889844af15
Fightingkeyboard <anthony.chen28@myhunter.cuny.edu>
2017-10-26 09:58:45 -0400

    10/26/2017

--- 09/problem1.py
+++ 09/problem1.py
@@ -4,7 +4,7 @@ how many people to invite if cake is u units and everyone needs A/B units'''
 
 def divide(A,B,u):
     recieved = A / B
-    return int5(u // recieved)
+    return int(u // recieved)
 
 print (divide(5,10,1))
 print (divide(5,10,2))
